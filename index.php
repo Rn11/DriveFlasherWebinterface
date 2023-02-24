a17748e99dc4df7ef8ee854a7b96093a9f32d577
Rn11 <Rn11@users.noreply.github.com>
2023-02-24 08:56:52 +0100

    Added format section

--- index.php
+++ index.php
@@ -8,24 +8,30 @@
 </head>
 
 <body>
+  <div class="mode-selection">
+    <div class="file-upload-container">
+      <h4 class="headline">Select a file to upload</h4>
+      <div id="selected-file-name">No file selected</div>
+      <form id="file-upload" method=POST enctype=multipart/form-data onsubmit="uploadFile()">
+        <div class="form-group">
+          <input type="file" name="file" id="file" onchange="updateSelectedFile(this)"
+            accept=".iso,.img,.zip,.usb,.bz2,.bzip2,.gz,.vhd,.gz">
+          <label for="file">Choose a file</label>
+
+          <div type="submit" form="file-upload" class="progress-btn" data-progress-style="fill-back">
+            <div class="btn">Upload</div>
+            <div class="progress"></div>
+          </div>
+      </form>
+    </div>
+  </div>
 
   <div class="file-upload-container">
-    <h4 class="headline">Select a file to upload</h4>
-    <div id="selected-file-name">No file selected</div>
-    <form id="file-upload" method=POST enctype=multipart/form-data onsubmit="uploadFile()">
-      <div class="form-group">
-        <input type="file" name="file" id="file" onchange="updateSelectedFile(this)"
-          accept=".iso,.img,.zip,.usb,.bz2,.bzip2,.gz,.vhd,.gz">
-        <label for="file">Choose a file</label>
+    FORMAT DEVICE
+  </div>
+  </div>
+  </div>
 
-        <div type="submit" form="file-upload" class="progress-btn" data-progress-style="fill-back">
-          <div class="btn">Upload</div>
-          <div class="progress"></div>
-        </div>
-    </form>
-  </div>
-  </div>
-  </div>
 
   <div class="table-flash-container">
     <h4 class="headline">Select the drives to flash</h4>
@@ -50,8 +56,8 @@
       ?>
     </table>
     <div class="tooltip">
-    <button class="button btnFlash" type="button" disabled>Flash drive!</button>
-  <span class="tooltiptext">Upload an image and select a drive first!</span>
+      <button class="button btnFlash" type="button" disabled>Flash drive!</button>
+      <span class="tooltiptext">Upload an image and select a drive first!</span>
     </div>
   </div>
   <script src="./js/libs/jquery-2.1.3.min.js"></script>
